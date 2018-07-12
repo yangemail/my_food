159c43ab33f12b6fdbb2c42edb423c0367c60a11
Yang Zhang <>
2018-07-12 06:38:43 -0700

    Add details page.

--- src/controller/api.server.controller.js
+++ src/controller/api.server.controller.js
@@ -6,3 +6,7 @@ const path = require('path');
 exports.index = function (req, res, next) {
     res.render('api/index_index', {});
 };
+
+exports.recipeId = function (req, res, next) {
+    res.render('api/recipe_detail', {});
+};
--- src/router/api.server.router.js
+++ src/router/api.server.router.js
@@ -3,8 +3,14 @@
 const express = require('express');
 const router = express.Router();
 
+const asyncHandler = require('express-async-handler');
+
 module.exports = function (app) {
     const api = require('../controller/api.server.controller');
 
-    app.route('/').get(api.index);
+    app.use('/', router);
+
+    router.get('/', asyncHandler(api.index));
+
+    router.get('/recipe/:id', asyncHandler(api.recipeId));
 };
